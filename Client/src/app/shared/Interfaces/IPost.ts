848f8dd8f8372287c40de8e1c680dd32a52ef6e0
Dreed657 <ninjakiviWTF@gmail.com>
2021-02-26 17:51:15 +0200

    Fixed feed layout

--- Client/src/app/shared/Interfaces/IPost.ts
+++ Client/src/app/shared/Interfaces/IPost.ts
@@ -1,7 +1,9 @@
 export interface IPost {
     id: number;
+    userName: string;
     content: string;
     imageUrl: string;
+    createdAt: Date;
     likes: number;
     comments: number;
     isLiked: boolean;
