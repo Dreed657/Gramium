113ac345356c081138f17906194832b7698d5373
Dreed657 <ninjakiviWTF@gmail.com>
2021-04-13 15:11:11 +0300

    Time pipe fixed

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -2,20 +2,18 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
-import { TimeDiffPipe } from './time-diff.pipe';
 import { SearchComponent } from './search/search.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FilterPipe } from './filter.pipe';
-
-
+import { TimeAgoPipe } from './time-ago.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, TimeDiffPipe, SearchComponent, FilterPipe],
+  declarations: [LoaderComponent, SearchComponent, FilterPipe, TimeAgoPipe],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     MDBBootstrapModule.forRoot()
   ],
-  exports: [LoaderComponent, TimeDiffPipe, SearchComponent],
+  exports: [LoaderComponent, SearchComponent, TimeAgoPipe],
 })
 export class SharedModule { }
